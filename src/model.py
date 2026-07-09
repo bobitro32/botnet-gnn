2a9837d18dc842aeadf1fe781c6b95e5b261ea9a
bobitro32 <bobi.cyber12@gmail.com>
2026-07-09 12:31:37 +0300

    train Random Forest classifier

--- src/model.py
+++ src/model.py
@@ -19,3 +19,17 @@ def _split_data(features_df: pd.DataFrame):
     print(f"Test:  {len(X_test):,} реда")
 
     return X_train, X_test, y_train, y_test
+
+def _train_model(X_train, y_train) -> RandomForestClassifier:
+    """Обучава Random Forest класификатор."""
+    model = RandomForestClassifier(
+        n_estimators=100,       # 100 дървета
+        max_depth=10,            # максимална дълбочина на всяко дърво
+        random_state=42,
+        class_weight='balanced'  # компенсира class imbalance
+    )
+
+    model.fit(X_train, y_train)
+    print("Модел обучен!")
+
+    return model
